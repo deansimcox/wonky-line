faeb357676fcad392c6203590431464d973e313b
Corey Wolfhart <dean.simcox@hbf.com.au>
2025-04-11 15:35:58 +0800

    added github workflow to deploy ladle to github pages

--- src/WonkyLine/WonkyLine.stories.tsx
+++ src/WonkyLine/WonkyLine.stories.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { WonkyLine } from "./WonkyLine.js";
 
 export const WonkyLineStory = () => (
-  <h1>
-    <WonkyLine>Hello world</WonkyLine>
-  </h1>
+	<h1>
+		<WonkyLine>Hello world</WonkyLine>
+	</h1>
 );
